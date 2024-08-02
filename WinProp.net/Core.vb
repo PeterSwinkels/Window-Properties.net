521985447fddfed60a87b3885b8a8461a3f82f15
Peter Swinkels <peter_swinkels@hotmail.com>
2024-08-02 11:00:48 +0200

    Add files via upload

--- WinProp.net/Core.vb
+++ WinProp.net/Core.vb
@@ -9,6 +9,7 @@ Imports System
 Imports System.Collections.Generic
 Imports System.Convert
 Imports System.Diagnostics
+Imports System.Environment
 Imports System.Runtime.InteropServices
 Imports System.Runtime.InteropServices.Marshal
 Imports System.Text
@@ -48,7 +49,7 @@ Public Module CoreModule
    Private Delegate Function EnumWindowsProc(ByVal hWnd As Integer, ByVal lParam As Integer) As Integer
    Private Delegate Function PropEnumProcEx(ByVal hwnd As Integer, ByVal lpszString As IntPtr, ByVal hData As IntPtr, ByVal dwData As Integer) As Integer
 
-   'This structure defines a property associated with a window.
+   'This structure defines a window's property.
    Private Structure WindowPropertyStr
       Public Name As String    'Defines a property's name.
       Public Value As String   'Defines a property's value.
@@ -58,146 +59,223 @@ Public Module CoreModule
 
    'This procedure is executed when this program is started.
    Public Sub Main()
-      EnumWindows(AddressOf HandleWindow, CInt(0))
+      Try
+         EnumWindows(AddressOf HandleWindow, CInt(0))
+      Catch ExceptionO As Exception
+         DisplayError(ExceptionO)
+      End Try
+   End Sub
+
+   'This procedure displays any errors that occur.
+   Private Sub DisplayError(ExceptionO As Exception)
+      Try
+         Console.ForegroundColor = ConsoleColor.Red
+         Console.Error.WriteLine($"{NewLine}ERROR: {ExceptionO.Message}{NewLine}")
+      Catch
+         [Exit](0)
+      End Try
    End Sub
 
    'This procedure displays the specified window's information.
    Private Sub DisplayWindowInformation(WindowH As Integer)
-      Console.ForegroundColor = ConsoleColor.Yellow
-      Console.WriteLine(GetWindowProcessPath(WindowH))
-      Console.ForegroundColor = ConsoleColor.Green
-      Console.WriteLine(GetWindowPath(WindowH))
+      Try
+         Console.ForegroundColor = ConsoleColor.Yellow
+         Console.WriteLine(GetWindowProcessPath(WindowH))
+         Console.ForegroundColor = ConsoleColor.Green
+         Console.WriteLine(GetWindowPath(WindowH))
+      Catch ExceptionO As Exception
+         DisplayError(ExceptionO)
+      End Try
    End Sub
 
    'This procedure displays the specified window properties.
    Private Sub DisplayWindowProperties(Properties As List(Of WindowPropertyStr))
-      Console.ForegroundColor = ConsoleColor.Cyan
-      WindowProperties.ForEach(Sub([Property]) Console.WriteLine($"""{[Property].Name}"" = ""{[Property].Value}"""))
+      Try
+         Console.ForegroundColor = ConsoleColor.Cyan
+         WindowProperties.ForEach(Sub([Property]) Console.WriteLine($"""{[Property].Name}"" = ""{[Property].Value}"""))
+      Catch ExceptionO As Exception
+         DisplayError(ExceptionO)
+      End Try
    End Sub
 
    'This procedure converts non-displayable or all (if specified) characters in the specified text to escape sequences.
    Public Function Escape(ToEscape As String, Optional EscapeCharacter As Char = "/"c) As String
-      Dim Character As New Char
-      Dim Escaped As New StringBuilder
-      Dim Index As Integer = 0
-      Dim Text As String = If(ToEscape Is Nothing, "", ToEscape)
+      Try
+         Dim Character As New Char
+         Dim Escaped As New StringBuilder
+         Dim Index As Integer = 0
+         Dim Text As String = If(ToEscape Is Nothing, "", ToEscape)
 
-      With Escaped
-         Do Until Index >= Text.Length
-            Character = Text.Chars(Index)
+         With Escaped
+            Do Until Index >= Text.Length
+               Character = Text.Chars(Index)
 
-            If Character = EscapeCharacter Then
-               .Append(New String(EscapeCharacter, 2))
-            ElseIf (Character = ControlChars.Tab OrElse (Character >= " "c AndAlso Character <= "~"c)) Then
-               .Append(Character)
-            Else
-               .Append($"{EscapeCharacter}{ToInt32(Character):X4}")
-            End If
-            Index += 1
-         Loop
-      End With
+               If Character = EscapeCharacter Then
+                  .Append(New String(EscapeCharacter, 2))
+               ElseIf (Character = ControlChars.Tab OrElse (Character >= " "c AndAlso Character <= "~"c)) Then
+                  .Append(Character)
+               Else
+                  .Append($"{EscapeCharacter}{ToInt32(Character):X4}")
+               End If
+               Index += 1
+            Loop
+         End With
 
-      Return Escaped.ToString()
+         Return Escaped.ToString()
+      Catch ExceptionO As Exception
+         DisplayError(ExceptionO)
+      End Try
+
+      Return Nothing
    End Function
 
    'This procedure returns the specified window's class.
    Private Function GetWindowClass(WindowH As Integer) As String
-      Dim Buffer As IntPtr = AllocHGlobal(UShort.MaxValue)
-      Dim Length As Integer = CInt(GetClassNameW(WindowH, Buffer, UShort.MaxValue))
-      Dim WindowClass As String = If(Length > 0, PtrToStringUni(Buffer).Substring(0, Length), Nothing)
+      Try
+         Dim Buffer As IntPtr = AllocHGlobal(UShort.MaxValue)
+         Dim Length As Integer = CInt(GetClassNameW(WindowH, Buffer, UShort.MaxValue))
+         Dim WindowClass As String = If(Length > 0, PtrToStringUni(Buffer).Substring(0, Length), Nothing)
 
-      FreeHGlobal(Buffer)
+         FreeHGlobal(Buffer)
 
-      Return WindowClass
+         Return WindowClass
+      Catch ExceptionO As Exception
+         DisplayError(ExceptionO)
+      End Try
+
+      Return Nothing
    End Function
 
    'This procedure returns the specified window's class and text preceded by any parent windows' information.
    Private Function GetWindowPath(WindowHandle As Integer) As String
-      Dim WindowPath As New StringBuilder
+      Try
+         Dim WindowPath As New StringBuilder
 
-      Do Until WindowHandle = 0
-         If WindowPath.Length > 0 Then WindowPath.Append("\")
-         WindowPath.Append($"""{Escape(GetWindowText(WindowHandle)).Trim()}""(""{Escape(GetWindowClass(WindowHandle)).Trim()}"")")
-         WindowHandle = GetParent(WindowHandle)
-      Loop
+         Do Until WindowHandle = 0
+            If WindowPath.Length > 0 Then WindowPath.Append("\")
+            WindowPath.Append($"""{Escape(GetWindowText(WindowHandle)).Trim()}""(""{Escape(GetWindowClass(WindowHandle)).Trim()}"")")
+            WindowHandle = GetParent(WindowHandle)
+         Loop
 
-      Return WindowPath.ToString()
+         Return WindowPath.ToString()
+      Catch ExceptionO As Exception
+         DisplayError(ExceptionO)
+      End Try
+
+      Return Nothing
    End Function
 
    'This procedure returns the specified window's process path.
    Private Function GetWindowProcessPath(WindowH As Integer) As String
-      Dim ProcessId As New Integer
+      Try
+         Dim ProcessId As New Integer
 
-      GetWindowThreadProcessId(WindowH, ProcessId)
+         GetWindowThreadProcessId(WindowH, ProcessId)
 
-      Return Process.GetProcessById(ProcessId).MainModule.FileName
+         Return Process.GetProcessById(ProcessId).MainModule.FileName
+      Catch ExceptionO As Exception
+         DisplayError(ExceptionO)
+      End Try
+
+      Return Nothing
    End Function
 
    'This procedure returns the specified window's text.
    Private Function GetWindowText(WindowH As Integer) As String
-      Dim Buffer As New IntPtr
-      Dim Length As New Integer
-      Dim PasswordCharacter As New Integer
-      Dim WindowText As String = Nothing
+      Try
+         Dim Buffer As New IntPtr
+         Dim Length As New Integer
+         Dim PasswordCharacter As New Integer
+         Dim WindowText As String = Nothing
 
-      If WindowHasStyle(WindowH, ES_PASSWORD) Then
-         PasswordCharacter = CInt(SendMessageW(WindowH, EM_GETPASSWORDCHAR, Nothing, Nothing))
-         If Not PasswordCharacter = Nothing Then
-            PostMessageA(WindowH, EM_SETPASSWORDCHAR, Nothing, Nothing)
-            Sleep(1000)
+         If WindowHasStyle(WindowH, ES_PASSWORD) Then
+            PasswordCharacter = CInt(SendMessageW(WindowH, EM_GETPASSWORDCHAR, Nothing, Nothing))
+            If Not PasswordCharacter = Nothing Then
+               PostMessageA(WindowH, EM_SETPASSWORDCHAR, Nothing, Nothing)
+               Sleep(1000)
+            End If
          End If
-      End If
 
-      Length = CInt(SendMessageW(WindowH, WM_GETTEXTLENGTH, Nothing, Nothing)) + 1
-      Buffer = AllocHGlobal(UShort.MaxValue)
-      Length = CInt(SendMessageW(WindowH, WM_GETTEXT, Length, Buffer))
-      WindowText = PtrToStringUni(Buffer)
-      FreeHGlobal(Buffer)
+         Length = CInt(SendMessageW(WindowH, WM_GETTEXTLENGTH, Nothing, Nothing)) + 1
+         Buffer = AllocHGlobal(UShort.MaxValue)
+         Length = CInt(SendMessageW(WindowH, WM_GETTEXT, Length, Buffer))
+         WindowText = PtrToStringUni(Buffer)
+         FreeHGlobal(Buffer)
 
-      WindowText = If(Length <= WindowText.Length, WindowText.Substring(0, Length), Nothing)
+         WindowText = If(Length <= WindowText.Length, WindowText.Substring(0, Length), Nothing)
 
-      If Not PasswordCharacter = Nothing Then PostMessageA(WindowH, EM_SETPASSWORDCHAR, PasswordCharacter, Nothing)
+         If Not PasswordCharacter = Nothing Then PostMessageA(WindowH, EM_SETPASSWORDCHAR, PasswordCharacter, Nothing)
 
-      Return WindowText
+         Return WindowText
+      Catch ExceptionO As Exception
+         DisplayError(ExceptionO)
+      End Try
+
+      Return Nothing
    End Function
 
    'This procedure handles the specified child window.
    Private Function HandleChildWindow(hWnd As Integer, lParam As Integer) As Integer
-      WindowProperties.Clear()
-      EnumPropsExA(hWnd, AddressOf HandleWindowProperty, CInt(0))
-      If WindowProperties.Count > 0 Then
-         Console.WriteLine()
-         DisplayWindowInformation(hWnd)
-         DisplayWindowProperties(WindowProperties)
-      End If
+      Try
+         WindowProperties.Clear()
+         EnumPropsExA(hWnd, AddressOf HandleWindowProperty, CInt(0))
+         If WindowProperties.Count > 0 Then
+            Console.WriteLine()
+            DisplayWindowInformation(hWnd)
+            DisplayWindowProperties(WindowProperties)
+         End If
+
+         Return CInt(True)
+      Catch ExceptionO As Exception
+         DisplayError(ExceptionO)
+      End Try
 
       Return CInt(True)
    End Function
 
    'This procedure handles the specified window.
    Private Function HandleWindow(hWnd As Integer, lParam As Integer) As Integer
-      WindowProperties.Clear()
-      EnumPropsExA(hWnd, AddressOf HandleWindowProperty, CInt(0))
-      If WindowProperties.Count > 0 Then
-         Console.WriteLine()
-         DisplayWindowInformation(hWnd)
-         DisplayWindowProperties(WindowProperties)
-      End If
+      Try
+         WindowProperties.Clear()
 
-      EnumChildWindows(hWnd, AddressOf HandleChildWindow, CInt(0))
+         EnumPropsExA(hWnd, AddressOf HandleWindowProperty, CInt(0))
+         If WindowProperties.Count > 0 Then
+            Console.WriteLine()
+            DisplayWindowInformation(hWnd)
+            DisplayWindowProperties(WindowProperties)
+         End If
+
+         EnumChildWindows(hWnd, AddressOf HandleChildWindow, CInt(0))
+
+         Return CInt(True)
+      Catch ExceptionO As Exception
+         DisplayError(ExceptionO)
+      End Try
 
       Return CInt(True)
    End Function
 
    'This procedure handles the specified window property.
    Private Function HandleWindowProperty(ByVal hwnd As Integer, ByVal lpszString As IntPtr, ByVal hData As IntPtr, ByVal dwData As Integer) As Integer
-      WindowProperties.Add(New WindowPropertyStr With {.Name = PtrToStringAnsi(lpszString), .Value = Escape(PtrToStringAnsi(hData))})
+      Try
+         WindowProperties.Add(New WindowPropertyStr With {.Name = PtrToStringAnsi(lpszString), .Value = Escape(PtrToStringAnsi(hData))})
+
+         Return CInt(True)
+      Catch ExceptionO As Exception
+         DisplayError(ExceptionO)
+      End Try
 
       Return CInt(True)
    End Function
 
    'This procedure returns the checks whether a window has the specified style and returns the result.
    Private Function WindowHasStyle(WindowH As Integer, Style As Integer) As Boolean
-      Return (CInt(GetWindowLongA(WindowH, GWL_STYLE)) And Style) = Style
+      Try
+         Return (CInt(GetWindowLongA(WindowH, GWL_STYLE)) And Style) = Style
+      Catch ExceptionO As Exception
+         DisplayError(ExceptionO)
+      End Try
+
+      Return Nothing
    End Function
 End Module
